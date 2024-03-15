f6f10b98c9b6586d08afcce6ebe2c7b5b9d9ebd2
Mayappa <mayappapujari561999@gmail.com>
2024-03-15 20:43:00 +0530

    added some more products

--- Registration_Form/init/data.js
+++ Registration_Form/init/data.js
@@ -42,6 +42,135 @@ const sampleProducts = [
     price: 100,
     category: "Food & Groceries",
   },
+  {
+    image: {
+      url: "https://5.imimg.com/data5/MM/BT/OD/SELLER-48652903/glass-bottels-200-ml-coke-thums-up-limca-sprite-fanta-500x500.jpg",
+      filename: "Coca-Cola",
+    },
+    name: "Coca-Cola",
+    description:
+      "The iconic fizzy drink loved worldwide, offering a refreshing blend of cola flavor.",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://m.media-amazon.com/images/I/41eN+JCtNiL.jpg",
+      filename: "Corn Flakes",
+    },
+    name: "Kellogg's Corn Flakes",
+    description:
+      "Crunchy, golden flakes made from corn, a nutritious breakfast option.",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://m.media-amazon.com/images/I/813axPlVxBL.jpg",
+      filename: "Potato Chips",
+    },
+    name: "Lays Potato Chips",
+    description: "Crispy, flavorful, classic snack. Enjoy and take taste",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://5.imimg.com/data5/ANDROID/Default/2022/9/JT/VK/WC/161121084/product-jpeg.jpg",
+      filename: "Maggie",
+    },
+    name: "Maggie instant Noodles",
+    description:
+      "A quick and convenient meal option loved by all ages, offering delicious noodles in a variety of flavors.",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://5.imimg.com/data5/MM/BT/OD/SELLER-48652903/glass-bottels-200-ml-coke-thums-up-limca-sprite-fanta-500x500.jpg",
+      filename: "Coca-Cola",
+    },
+    name: "Coca-Cola",
+    description:
+      "The iconic fizzy drink loved worldwide, offering a refreshing blend of cola flavor.",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://m.media-amazon.com/images/I/41eN+JCtNiL.jpg",
+      filename: "Corn Flakes",
+    },
+    name: "Kellogg's Corn Flakes",
+    description:
+      "Crunchy, golden flakes made from corn, a nutritious breakfast option.",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://m.media-amazon.com/images/I/813axPlVxBL.jpg",
+      filename: "Potato Chips",
+    },
+    name: "Lays Potato Chips",
+    description: "Crispy, flavorful, classic snack. Enjoy and take taste",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://5.imimg.com/data5/ANDROID/Default/2022/9/JT/VK/WC/161121084/product-jpeg.jpg",
+      filename: "Maggie",
+    },
+    name: "Maggie instant Noodles",
+    description:
+      "A quick and convenient meal option loved by all ages, offering delicious noodles in a variety of flavors.",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://5.imimg.com/data5/MM/BT/OD/SELLER-48652903/glass-bottels-200-ml-coke-thums-up-limca-sprite-fanta-500x500.jpg",
+      filename: "Coca-Cola",
+    },
+    name: "Coca-Cola",
+    description:
+      "The iconic fizzy drink loved worldwide, offering a refreshing blend of cola flavor.",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://m.media-amazon.com/images/I/41eN+JCtNiL.jpg",
+      filename: "Corn Flakes",
+    },
+    name: "Kellogg's Corn Flakes",
+    description:
+      "Crunchy, golden flakes made from corn, a nutritious breakfast option.",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://m.media-amazon.com/images/I/813axPlVxBL.jpg",
+      filename: "Potato Chips",
+    },
+    name: "Lays Potato Chips",
+    description: "Crispy, flavorful, classic snack. Enjoy and take taste",
+    price: 100,
+    category: "Food & Groceries",
+  },
+  {
+    image: {
+      url: "https://5.imimg.com/data5/ANDROID/Default/2022/9/JT/VK/WC/161121084/product-jpeg.jpg",
+      filename: "Maggie",
+    },
+    name: "Maggie instant Noodles",
+    description:
+      "A quick and convenient meal option loved by all ages, offering delicious noodles in a variety of flavors.",
+    price: 100,
+    category: "Food & Groceries",
+  },
 ];
 
 module.exports = { data: sampleProducts };
