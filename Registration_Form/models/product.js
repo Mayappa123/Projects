4fecb9350adf5d9acd299926c4d5e2ba274ac018
Mayappa <mayappapujari561999@gmail.com>
2024-03-21 21:09:22 +0530

    tony

--- Registration_Form/models/product.js
+++ Registration_Form/models/product.js
@@ -1,3 +1,5 @@
+// product.js
+
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
--- Registration_Form/models/user.js
+++ Registration_Form/models/user.js
@@ -1,4 +1,4 @@
-
+// usre.js
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
