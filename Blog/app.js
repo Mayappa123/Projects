02fe5b227e79f89aa8c11ab92b6630bfc37705d4
Mayappa <mayappapujari561999@gmail.com>
2024-03-25 13:22:20 +0530

    started blog website

--- Blog/app.js
+++ Blog/app.js
@@ -1,7 +1,49 @@
-const express = require("express");
-const app = express();
 
+
+
+// app.js o
+const express = require('express');
+
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const authRoutes = require('./routes/auth'); // Import
+
+const app = express();
 const port = 8040;
+
+// Passport middleware
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/", authRoutes);
+
+// Passport local strategy
+passport.use(new LocalStrategy({ usernameField: 'username' }, (username, password, done) => {
+  User.findOne({ username: username }, (err, user) => {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+    if (user.password !== password) { return done(null, false); }
+    return done(null, user);
+  });
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.listen(port, () => {
   console.log("app is running...");
