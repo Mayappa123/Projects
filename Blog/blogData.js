ca0fc26e7c3e6bb335b7c3909dbc6c735def309d
Mayappa <mayappapujari561999@gmail.com>
2024-03-25 12:54:38 +0530

    created basic structure of folders

---
+++ Blog/blogData.js
@@ -0,0 +1,191 @@
+const sampleProducts = [
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "React JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Full Stack",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Laravel",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+  {
+    name: ["author1.jpg", "author2.jpg"],
+    date: "05-06-2023",
+    subject: "Node JS",
+    title: "31 remarkable NPM packages to steramline your development",
+    author: ["Tony Stark", "John Doe"],
+    content:
+      "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
+  },
+];
