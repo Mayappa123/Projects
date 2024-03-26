6ed64bf7857c4182720ef6ae08436f360882f2f6
Mayappa <mayappapujari561999@gmail.com>
2024-03-26 20:01:22 +0530

    started controllers chapter

--- Blog/controllers/blogs.js
+++ Blog/controllers/blogs.js
@@ -1,1 +1,24 @@
 const Blog = require("../models/blog");
+
+module.exports.index = async(req, res) => {
+    const allBlogs = await Blog.find({});
+    res.render("./blogs/index.ejs");
+}
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("./blogs/new.ejs");
+};
+
+module.exports.showBlog = async (req, res) => {
+  let { id } = req.params;
+  const blog = await Blog.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("owner");
+  if (!blog) {
+    req.flash("error", "Listing you requested for does not exists..");
+    res.redirect("/blogs");
+  }
+  console.log(blog);
+  res.render("./blogs/show.ejs", {blog});
+};
+
--- Blog/routes/blog.js
+++ Blog/routes/blog.js
@@ -7,4 +7,35 @@ const multer = require("multer");
 const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
 
-router.get("/new", isLoggedIn, listingController.renderNewForm);
+router.get("/new", isLoggedIn, blogController.renderNewForm);
+
+router
+  .route("/")
+  .get(WrapAsync(blogController.index)) //index route
+  .post(
+    isLoggedIn,
+    upload.single("blog[image]"),
+    ValidateBlog,
+    WrapAsync(blogController.createBlog)
+  ); //create route
+
+router
+  .route("/:id")
+  .get(WrapAsync(blogController.showBlog)) //show route
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("blog[image]"),
+    ValidateBlog,
+    WrapAsync(blogController.updateBlog)
+  ) //update route
+  .delete(isLoggedIn, isOwner, WrapAsync(blogController.destroyBlog)); //delete route
+
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  WrapAsync(blogController.renderEditForm)
+); //edit route
+
+module.exports = router;
