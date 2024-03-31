17f8892288cb4be92f694f0cb5e4ab87d1bbadd4
Mayappa <mayappapujari561999@gmail.com>
2024-03-31 13:55:46 +0530

    changed some data from smapleData.js

--- Blog/init/blogData.js
+++ Blog/init/blogData.js
@@ -1,9 +1,11 @@
+// blogData.js
+
 const sampleBlogs = [
   {
     profileImage: "author1.jpg",
     date: "05-06-2023",
     subject: "Node JS",
-    title: "31 remarkable NPM packages to steramline your development",
+    title: "21 remarkable NPM packages to steramline your development",
     authorName: "Tony Stark",
     content:
       "This code creates a simple blog post layout with a subject, subtitle, and content area. The design is clean and minimalistic, making it easy to read. You can use this as a template and further customize it according to your preferences.",
