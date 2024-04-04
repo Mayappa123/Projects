08f06fd1d5768966169eb316dc03c986f4438c96
Mayappa <mayappapujari561999@gmail.com>
2024-04-04 17:25:58 +0530

    tring to create modules using MVC

--- Blog/app.js
+++ Blog/app.js
@@ -1,21 +1,19 @@
 // app.js
+
 const express = require("express");
-
-const session = require("express-session");
-const path = require("path");
-const bodyParser = require("body-parser");
-const sampleBlogs = require("./init/blogData");
-const ejsMate = require("ejs-mate");
-const initDB = require("./init");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const User = require("./models/user");
-const flash = require("connect-flash");
-const Blog = require("./models/blog");
-const methodOverride = require("method-override");
-const { isLoggedin, isOwner } = require("./middleware.js");
-
 const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const { main } = require("./init/index.js");
+const passport = require("passport");
+const User = require("./models/user");
+const session = require("express-session");
+const flash = require("connect-flash");
+const Blog = require("./models/blog.js");
+const methodOverride = require("method-override");
+const { isLoggedin, isOwner , saveRedirectUrl } = require("./middleware.js");
+const LocalStrategy = require("passport-local").Strategy;
+
 const port = 8040;
 
 app.set("view engine", "ejs");
@@ -26,14 +24,20 @@ app.use(methodOverride("_method"));
 app.use(express.static(__dirname));
 app.engine("ejs", ejsMate);
 
-// Passport middleware
 const sessionOptions = {
   secret: "mayappa",
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
+
 app.use(session(sessionOptions));
 app.use(flash());
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -48,70 +52,89 @@ app.use((req, res, next) => {
   next();
 });
 
-initDB();
+main();
 
-app.post("/signup", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const newUser = new User({
-      username,
-      email,
-      password,
-    });
-    const registeredUser = await User.register(newUser, password);
-    req.login(registeredUser, (err) => {
-      if (err) {
-        return next(err);
-      }
-      req.flash("success", "Welcome to Blogify");
-      res.redirect("/blogs");
-    });
-  } catch (error) {
-    req.flash("errror", "something went wrong");
-    console.log("error : ", error);
-  }
+//Users related routes...
+app.get("/login", (req, res) => {
+  res.render("users/login.ejs");
 });
 
 app.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
-    req.flash("success", "welcome to Blogify...");
-    await res.redirect("/blogs");
+    req.flash("success", "Welcome back on Blogify!");
+    let redirectUrl = res.locals.redirectUrl || "/blogs";
+    res.redirect(redirectUrl);
   }
 );
-
-app.get("/logout", isLoggedin, (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      req.flash("error");
-      return next(err);
-    }
-    req.flash("success", "You are logged out now...");
-    res.redirect("/login");
-  });
-});
-
-app.get("/blogs", isLoggedin, async (req, res) => {
-  const blogs = await Blog.find({});
-  res.render("blogs/blogs", { blogData: blogs });
-});
-
-app.get("/", (req, res) => {
-  res.render("users/login.ejs");
-});
-
-app.get("/login", (req, res) => {
-  res.render("users/login.ejs");
-});
 
 app.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
 
+app.post("/signup", async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next();
+      }
+      req.flash("success", "Welcome to Blogify!!!");
+      res.redirect("/blogs");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+});
+
+app.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    } else {
+      req.flash("success", "You are logged out now...");
+      res.redirect("/login");
+    }
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("blogs/contact.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("blogs/about.ejs");
+});
+
+app.get("/user/active", (req, res) => {
+  res.render("users/activeUser.ejs", { currUser: req.user });
+});
+
+
+
+
+//Blogs related routes...
+//index route
+app.get("/blogs", async (req, res) => {
+  const blogs = await Blog.find({});
+  res.render("blogs/blogs", { blogData: blogs });
+});
+
+//new route
+app.get("/blogs/new", isLoggedin, (req, res) => {
+  res.render("blogs/new.ejs");
+});
+
+//show route
 app.get("/blogs/:id", isLoggedin, async (req, res) => {
   let { id } = req.params;
   const blog = await Blog.findById(id).populate("owner");
@@ -122,11 +145,7 @@ app.get("/blogs/:id", isLoggedin, async (req, res) => {
   res.render("blogs/show.ejs", { blog });
 });
 
-app.get("/blog/new", isLoggedin, (req, res) => {
-  res.render("blogs/new.ejs");
-});
-
-//Create route
+//create route
 app.post("/newBlog", isLoggedin, async (req, res) => {
   const newBlog = new Blog(req.body.blog);
   newBlog.owner = req.user._id;
@@ -143,17 +162,23 @@ app.get("/blogs/:id/edit", isLoggedin, isOwner, async (req, res) => {
   res.render("blogs/edit.ejs", { blog });
 });
 
-//Update route
+//update route
 app.put("/blogs/:id", isLoggedin, isOwner, async (req, res) => {
   try {
     const { id } = req.params;
-    const updatedBlog = await Blog.findByIdAndUpdate(id, {...req.body.blog}, {
-      new: true,
-    });
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      { ...req.body.blog },
+      {
+        new: true,
+        useFindAndModify: false,
+      }
+    );
     if (!updatedBlog) {
       req.flash("error", "Blog not found.");
       return res.redirect("/blogs");
     }
+    console.log(updatedBlog);
     req.flash("success", "Blog updated successfully.");
     res.redirect(`/blogs/${id}`);
   } catch (error) {
@@ -163,28 +188,7 @@ app.put("/blogs/:id", isLoggedin, isOwner, async (req, res) => {
   }
 });
 
-//Update route
-// app.put("/blogs/:id", isLoggedin, isOwner, async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const updatedBlog = await Blog.findByIdAndUpdate(
-//       id,
-//       { ...req.body.blog },
-//       { new: true }
-//     );
-//     await updatedBlog.save();
-//     req.flash("success", "blog updated successfully...");
-//     res.redirect(`/blogs/${id}`);
-//   }
-//   catch (error) {
-//     console.log(error)
-//   }
-// });
-
-
-
-
-// Delete route
+//delete blog
 app.delete("/blogs/:id", isLoggedin, isOwner, async (req, res) => {
   try {
     const { id } = req.params;
@@ -196,34 +200,21 @@ app.delete("/blogs/:id", isLoggedin, isOwner, async (req, res) => {
   }
 });
 
-app.get("/contact", isLoggedin, (req, res) => {
-  res.render("blogs/contact.ejs");
-});
-
-app.get("/about", isLoggedin, (req, res) => {
-  res.render("blogs/about.ejs");
-});
-
-app.get("/user/active", isLoggedin, (req, res) => {
-  res.render("users/activeUser.ejs", { currUser: req.user });
-});
-
-app.post("/updateProfile", isLoggedin, async (req, res) => {
-  const { username, email, contact } = req.body;
+//search blog
+app.get("/:id/search", isLoggedin, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id);
-    user.username = username;
-    user.email = email;
-    user.contact = contact;
+    const { id } = req.params;
+    const { search } = req.query;
 
-    if (req.file) {
-      user.profileImage = req.file.filename;
-    }
-    await user.save();
-    res.redirect("/user/active");
+    const blogs = await Blog.find({
+      owner: mongoose.Types.ObjectId(id),
+      subject: { $regex: new RegExp(search, "i") },
+    });
+
+    res.render("blogs/blogs.ejs", { blogs });
   } catch (error) {
-    console.error("Error updating profile:", error);
-    res.status(500).send("Error updating profile");
+    console.error("Error searching blogs:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
